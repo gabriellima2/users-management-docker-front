7003a138bc060bc44c8c6b44abb24fe43cab2983
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-27 14:59:35 -0300

    feat: add use-fetch composable

---
+++ src/composables/use-fetch.ts
@@ -0,0 +1,25 @@
+import { ref, onMounted, type UnwrapRef } from 'vue'
+
+type UseFetchParams<D> = {
+	service: () => Promise<D>
+}
+
+export function useFetch<D>(params: UseFetchParams<D>) {
+	const { service } = params;
+	const data = ref<null | D>(null)
+	const error = ref<null | string>(null)
+	const isLoading = ref<boolean>(true)
+	
+	onMounted(() => {
+		service()
+			.then((value) => (data.value = value as UnwrapRef<D>))
+			.catch((err) => (error.value = err))
+			.finally(() => (isLoading.value = false))
+	})
+
+	return {
+		data,
+		error,
+		isLoading,
+	}
+}
